6fd0d98fabf4234b858c56946d9cb8e84fc50591
Bobby Mantoni <bobby@mantoni.me>
2019-12-27 00:46:35 +0000

    more tests, mock canvas

--- football-frontend/src/Pitch.test.js
+++ football-frontend/src/Pitch.test.js
@@ -3,7 +3,25 @@ import ReactDOM from "react-dom";
 import { render } from '@testing-library/react';
 import Pitch from './Pitch';
 
-test('state updates get applied', () => {
+test('scores initialise properly', () => {
+    var p = new Pitch({});
+    expect(p.state.player1Score).toBe(0);    
+    expect(p.state.player2Score).toBe(0);
+});
+
+test('correct score gets incremented on scoring', () => {
+    const domContainer = document.createElement('div');
+    var p = ReactDOM.render(<Pitch />, domContainer);
+
+    p.handlePlayerScored(1, p);
+    expect(p.state.player1Score).toBe(1);
+    expect(p.state.player2Score).toBe(0);
+    p.handlePlayerScored(2, p);
+    expect(p.state.player1Score).toBe(1);
+    expect(p.state.player2Score).toBe(1);
+})
+
+test('state updates get applied correctly', () => {
     var testState = {
         player1: {
             position: {x: 42, y: 42},
@@ -20,17 +38,13 @@ test('state updates get applied', () => {
             velocity: {x: 10, y: 30}
         }
     }
-    var p = new Pitch({});
-    //const { getByText } = render(<Pitch />);
-    //HTMLCanvasElement.prototype.getContext = jest.fn(() => {});
-    //const domContainer = document.createElement('div');
-    //var p = ReactDOM.render(<Pitch />, domContainer);
-    var state = {};
-    var mockSetState = jest.fn(() => {});
-    p.setState = mockSetState;
+
+    const domContainer = document.createElement('div');
+    var p = ReactDOM.render(<Pitch />, domContainer);
+
     p.updateStateHandler(testState, p);
-    expect(mockSetState.mock.calls.length).toBe(1);
-    expect(mockSetState.mock.calls[0][0]['player1Score']).toBe(12);
+
+    expect(p.state.player1Score).toBe(12);
     expect(p.player1.body.position.x).toBe(42);
     expect(p.player1.body.position.y).toBe(42);
     expect(p.player1.body.velocity.x).toBe(1);
@@ -39,7 +53,7 @@ test('state updates get applied', () => {
     expect(p.player2.body.position.y).toBe(420);
     expect(p.player2.body.velocity.x).toBe(10);
     expect(p.player2.body.velocity.y).toBe(30);
-    expect(mockSetState.mock.calls[0][0]['player2Score']).toBe(34);
+    expect(p.state.player2Score).toBe(34);
     expect(p.ball.body.position.x).toBe(420);
     expect(p.ball.body.position.y).toBe(420);
     expect(p.ball.body.velocity.x).toBe(10);
